1af7dd944ef35a8736c5272b42e36713147a034c
ipreuss <ilja.preuss@it-agile.de>
2024-03-21 22:07:36 +0100

    automgically create character deck

--- updateTTS.sh
+++ updateTTS.sh
@@ -3,21 +3,27 @@ savefile="/Users/ilja/Library/Tabletop Simulator/Saves/TS_Save_54.json"
 backupfile="./savefile_backup.json"
 templatefile="./template_workshop.json"
 luascript_start='"LuaScript":'
+
+echo "Backing up..."
 cp "$savefile" "$backupfile"
 
 # Push the backup file to the git repository
 git add "$backupfile"
-git commit -m "Update backup file"
-git push origin master
+git commit -q -m "Update backup file"
+git push -q origin master
 # Replace the value of "LuaScript" in the backup file with the Placeholder
+echo "creating template..."
 jq '.LuaScript = "Lua Script gets inserted here"' "$backupfile" > "$templatefile"
 cd ..
 rm -f bundle.*
+echo "Bundling..."
 luabundler bundle kdm/Global.ttslua \
 -p "?.ttslua" \
 -o bundle.lua
 
 # JSON-encode bundle.lua
+echo "JSON-encoding..."
 jq -Rs . bundle.lua > bundle.json
 
+echo "Inserting LuaScript..."
 jq --slurpfile luascript bundle.json '.LuaScript = $luascript[0]' "kdm/$templatefile" > "$savefile"
